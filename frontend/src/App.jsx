33532b1d620996ef153b453a9d4ff2baa3c6a729
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-02-20 20:23:03 +0530

    final project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import Editor from "react-simple-code-editor"
+import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [code, setCode] = useState(` function sum() {
+  return 1 + 1
+}`)
+
+  const [review, setReview] = useState('')
+  const [isLoading, setIsLoading] = useState(false) 
+  useEffect(() => {
+    prism.highlightAll()
+  }, [])
+
+  async function reviewCode() {
+    setIsLoading(true); 
+    setReview('Reviewing..'); 
+    try {
+      const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+      setReview(response.data) 
+    } catch (error) {
+      setReview('Failed to fetch review. Please try again later.') 
+    } finally {
+      setIsLoading(false); 
+    }
+  }
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
+                color: "#ccc",
+              }}
+            />
+          </div>
+          <div
+            onClick={reviewCode}
+            className="review">
+            {isLoading ? (
+              <span>Reviewing..</span>
+            ) : (
+              'Review'
+            )}
+          </div>
+        </div>
+        <div className="right">
+          <Markdown rehypePlugins={[rehypeHighlight]}>
+            {review}
+          </Markdown>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
