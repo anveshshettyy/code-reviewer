71db3c638db67a1d5ac062fab47d68db2956b7b4
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-02-21 20:14:42 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
-require('dotenv').config();
-const app = require('./src/app');
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
 
-const PORT = process.env.PORT || 3000;  
+const app = require("./src/app");
 
+// Serve Frontend (React Build)
+const frontendPath = path.join(__dirname, "frontend", "dist");
+
+app.use(express.static(frontendPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+// Start Server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
