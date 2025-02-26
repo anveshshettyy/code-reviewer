8612140ebd6157c23ce304bc9d41443178497f10
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-02-26 11:15:22 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,9 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+
 
 app.use('/ai', aiRoutes);
 
