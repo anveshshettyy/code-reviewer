a5af8a996a3adaae7b5b14484a387350f7d5c287
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-02-26 11:15:17 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,11 @@ const app = require("./src/app");
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "./frontend/dist")));  
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));  
 
 
-  app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, "./frontend/dist", "index.html"));
+  app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
